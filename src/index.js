6989164f0d1427690095f9abb670a30c111cf5c3
roman7501 <romanjeanelie@yahoo.fr>
2021-03-23 15:36:44 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 // here you put all the js you want.
-import Navbar from "./js/navbar.js";
+import lottie from "lottie-web";
+import data from "./js/animations/data.json";
 
-const navbar = new Navbar()
+const animationWrapper = document.getElementById("animation-wrapper");
 
+lottie.loadAnimation({
+  container: animationWrapper, // the dom element that will contain the animation
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  animationData: data,
+});
